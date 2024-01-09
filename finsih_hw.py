1c85ec55fd8ec365dd020da56ba5baddb101e797
Vladislav Nozhkin <mega.nozhkin@pm.me>
2024-01-09 14:35:22 +0300

    update finish_hw

--- finsih_hw.py
+++ finsih_hw.py
@@ -4,38 +4,40 @@ import csv
 def load_csv(file_path):
     with open(file_path, mode='r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
-        data = [row for row in csv_reader]
+        data = []
+        for row in csv_reader:
+            # Преобразуем возраст в целые числа
+            row['age'] = float(row['age']) if '.' in row['age'] else int(row['age'])
+            data.append(row)
     return data
-# {'name': 'Allison  Miss. Helen Loraine', 'device_type': 'laptop', 'browser': 'Firefox', 'sex': 'female', 'age': '48', 'bill': '1034', 'region': 'Montreal: PQ / Chesterville: ON'}
 
 
 def create_description(row):
-    gender = 'женского' if row['sex'] == 'female' else 'мужского'
-    description = f"Пользователь {row['name']} {gender} пола, {convert_years(row['age'])} совершил(а) покупку на {row['bill']} у.е. с {row['device_type']} браузера {row['browser']}. Регион, из которого совершалась покупка: {row['region']}."
+    # Создаём словарь для гендера
+    gender_map = {
+        'female': 'женского',
+        'male': 'мужского'
+    }
+    gender = gender_map.get(row['sex'], 'неопределенного')
+    description = (f"Пользователь {row['name']} {gender} пола, {convert_years(row['age'])} "
+                   f"совершил(а) покупку на {row['bill']} у.е. с {row['device_type']} браузера {row['browser']}. "
+                   f"Регион, из которого совершалась покупка: {row['region']}.")
     return description
 
 
 def convert_years(age):
-    if age.isdigit() == 1:
-        age = int(age)
-    else:
-        age = float(age)
-    suffix = 'лет'
-    if (age // 10) % 10 != 1:
-        if age % 10 == 1:
-            suffix = 'год'
-        elif age % 10 in (2, 3, 4):
-            suffix = 'года'
+    # Склоняем число в текстовом формате
+    suffix = 'лет' if 11 <= age % 100 <= 14 else {1: 'год', 2: 'года', 3: 'года', 4: 'года'}.get(age % 10, 'лет')
     return f'{age} {suffix}'
 
 
 def process_file(input_file, output_file):
+    # Читаем из csv и преобразуем в txt
     data = load_csv(input_file)
-    with open(output_file, mode='w', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file)
+    with open(output_file, mode='w', encoding='utf-8') as file:
         for row in data:
             description = create_description(row)
-            writer.writerow([description])
+            file.write(description + '\n')
 
 
 process_file("./web_clients_correct.csv", "load.txt")
