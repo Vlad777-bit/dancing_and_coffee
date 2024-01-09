d65ad98b091a8419a23bca261bd2f019f4172c75
Vladislav Nozhkin <mega.nozhkin@pm.me>
2024-01-09 15:13:04 +0300

    format code

--- finsih_hw.py
+++ finsih_hw.py
@@ -12,9 +12,11 @@ class CustomerDescriptionGenerator:
         with open(self.input_file, mode='r', encoding='utf-8') as file:
             csv_reader = csv.DictReader(file)
             data = []
+
             for row in csv_reader:
                 row['age'] = self._parse_age(row['age'])
                 data.append(row)
+
         return data
 
     @staticmethod
@@ -32,10 +34,15 @@ class CustomerDescriptionGenerator:
             'male': 'мужского'
         }
         gender = gender_map.get(row['sex'], 'неопределенного')
+
         age = CustomerDescriptionGenerator._convert_years(row['age'])
-        description = (f"Пользователь {row['name']} {gender} пола, {age} "
-                       f"совершил(а) покупку на {row['bill']} у.е. с {row['device_type']} браузера {row['browser']}. "
-                       f"Регион, из которого совершалась покупка: {row['region']}.")
+
+        description = (
+            f"Пользователь {row['name']} {gender} пола, {age} "
+            f"совершил(а) покупку на {row['bill']} у.е. с {row['device_type']} браузера {row['browser']}. "
+            f"Регион, из которого совершалась покупка: {row['region']}."
+        )
+
         return description
 
     @staticmethod
@@ -43,17 +50,23 @@ class CustomerDescriptionGenerator:
         """ Преобразует число лет в текстовый формат с правильным склонением. """
 
         suffix = 'лет' if 11 <= age % 100 <= 14 else {
-            1: 'год', 2: 'года', 3: 'года', 4: 'года'}.get(age % 10, 'лет')
+            1: 'год',
+            2: 'года',
+            3: 'года',
+            4: 'года'
+        }.get(age % 10, 'лет')
+
         return f'{age} {suffix}'
 
     def process_file(self):
         """ Читает данные из CSV-файла и записывает описания в TXT-файл. """
 
         data = self.load_csv()
+
         with open(self.output_file, mode='w', encoding='utf-8') as file:
             for row in data:
                 description = self._create_description(row)
-                file.write(description + '\n')
+                file.write(description + '\n\n')
 
 
 # Создаём инстанс класс
